2a2d71de891e8f02f5f4a825b69c7f0617a444d7
zhaoFangyi <fangyieagle@qq.com>
2020-01-29 15:41:53 +0800

    feat(all the file): transform data
    transform data and example test

--- src/helpers/util.ts
+++ src/helpers/util.ts
@@ -4,6 +4,10 @@ export function isDate(val: any): val is Date {
   return toString.call(val) === '[object Date]'
 }
 
+export function isPlainObject(val: any): val is Object {
+  return toString.call(val) === '[object Object]'
+}
+
 export function isObject(val: any): val is Object {
   return val !== null && typeof val === 'object'
 }
