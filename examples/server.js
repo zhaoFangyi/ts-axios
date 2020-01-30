1dbd8df59bc40fcd120b0ff3ee603ebac634d899
zhaoFangyi <fangyieagle@qq.com>
2020-01-30 15:10:49 +0800

    feat: catch error detail info

--- examples/server.js
+++ examples/server.js
@@ -35,6 +35,24 @@ router.get('/simple/get', function(req, res) {
 router.get('/base/get', function(req, res) {
   res.json(req.query)
 })
+router.get('/error/get', function(req, res) {
+  if (Math.random() > 0.5) {
+    res.json({
+      msg: 'hello world'
+    })
+  } else {
+    res.status(500)
+    res.end()
+  }
+})
+
+router.get('/error/timeout', function(req, res) {
+  setTimeout(() => {
+    res.json({
+      mes: 'hello timeout'
+    })
+  }, 3000)
+})
 
 router.post('/base/post', function (req, res) {
   res.json(req.body)
--- src/xhr.ts
+++ src/xhr.ts
@@ -1,18 +1,27 @@
 import { AxiosRequestConfig, AxiosPromise, AxiosResponse } from './types'
+import { parseHeaders } from './helpers/headers'
+import { createError } from './helpers/error'
 
 export default function xhr(config: AxiosRequestConfig): AxiosPromise {
-  return new Promise(resolve => {
-    const { data = null, url, method = 'get', headers, responseType } = config
+  return new Promise((resolve, reject) => {
+    const { data = null, url, method = 'get', headers, responseType, timeout } = config
 
     const request = new XMLHttpRequest()
 
     if (responseType) {
       request.responseType = responseType
     }
+    if (timeout) {
+      request.timeout = timeout
+    }
+
     request.open(method.toUpperCase(), url, true)
 
     request.onreadystatechange = function handleLoad() {
       if (request.readyState !== 4) {
+        return
+      }
+      if (request.status === 0) {
         return
       }
       const responseHeaders = parseHeaders(request.getAllResponseHeaders())
@@ -26,7 +35,14 @@ export default function xhr(config: AxiosRequestConfig): AxiosPromise {
         config,
         request
       }
-      resolve(response)
+      handleResponse(response)
+    }
+
+    request.onerror = function handleError() {
+      reject(createError('Network Error', config, null, request))
+    }
+    request.ontimeout = function handleTimeout() {
+      reject(createError(`Timeout of ${timeout}ms exceeded`, config, 'ECONNABORTED', request))
     }
     Object.keys(headers).forEach(name => {
       if (data === null && name.toLowerCase() === 'content-type') {
@@ -36,25 +52,20 @@ export default function xhr(config: AxiosRequestConfig): AxiosPromise {
       }
     })
     request.send(data)
-  })
-}
-
-export function parseHeaders(headers: string): any {
-  let parsed = Object.create(null)
-
-  if (!headers) {
-    return
-  }
-  headers.split('\r\n').forEach(line => {
-    let [key, val] = line.split(':')
-    key = key.trim().toLowerCase()
-    if (!key) {
-      return
+    function handleResponse(response: AxiosResponse) {
+      if (response.status >= 200 && response.status < 300) {
+        resolve(response)
+      } else {
+        reject(
+          createError(
+            `Request failed with status code${response.status}`,
+            config,
+            null,
+            request,
+            response
+          )
+        )
+      }
     }
-    if (val) {
-      val = val.trim()
-    }
-    parsed[key] = val
   })
-  return parsed
 }
