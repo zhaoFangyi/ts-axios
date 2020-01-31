4fea3bb8619d0e06a3ba5c6bd0b6d9ecf8f4fa87
zhaoFangyi <fangyieagle@qq.com>
2020-01-31 11:03:37 +0800

    feat: 泛型参数 和 demo

--- examples/extend/app.ts
+++ examples/extend/app.ts
@@ -8,6 +8,13 @@ axios({
   }
 })
 
+axios('/extend/post', {
+  method: 'post',
+  data: {
+    msg: 'I am overload axios'
+  }
+})
+
 axios.request({
   url: '/extend/post',
   method: 'post',
@@ -23,3 +30,42 @@ axios.head('/extend/head')
 axios.post('/extend/post', { msg: 'post'} )
 axios.put('/extend/put', { msg: 'put'} )
 axios.patch('/extend/patch', { msg: 'post'} )
+
+
+export interface ResponseData<T = any> {
+  /**
+   * 状态码
+   * @type {number}
+   */
+  code: number
+  /**
+   * 数据
+   * @type { T }
+   */
+  result: T
+  /**
+   * 消息
+   * @type { string }
+   */
+  message: string
+}
+
+interface User {
+  name: string
+  age: number
+}
+
+function getUser<T>() {
+  return axios<ResponseData<T>>('/extend/user')
+    .then(res => res.data)
+    .catch(err => console.log(err))
+}
+
+async function test() {
+  const user = await getUser<User>()
+  if (user) {
+    console.log(user.result.name)
+  }
+}
+
+test()
--- examples/server.js
+++ examples/server.js
@@ -108,6 +108,16 @@ function registerExtendRouter () {
   router.patch('/extend/patch', function(req, res) {
     res.json(req.body)
   })
+  router.get('/extend/user', function(req, res) {
+    res.json({
+      code: 0,
+      message: 'ok',
+      result: {
+        name: 'Jack',
+        age: 18,
+      }
+    })
+  })
 }
 app.use(router)
 
