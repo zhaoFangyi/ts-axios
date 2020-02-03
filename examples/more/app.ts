50a38ded420d7f2dc7f6b3ee58db85a52f4c5497
zhaoFangyi <fangyieagle@qq.com>
2020-02-03 17:08:38 +0800

    feat: authorization http

--- examples/more/app.ts
+++ examples/more/app.ts
@@ -77,3 +77,14 @@ uploadEl!.addEventListener('click', e=> {
   }
 })
 
+instance.post('/more/post', {
+  a: 1
+}, {
+  auth: {
+    username: 'Yee',
+    password: '123456'
+  }
+}).then(res => {
+  console.log(res)
+})
+
--- examples/server.js
+++ examples/server.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
 const multipart = require('connect-multiparty')
 const webpack = require('webpack')
+const atob = require('atob')
 const webpackDevMiddleware = require('webpack-dev-middleware')
 const webpackHotMiddleware = require('webpack-hot-middleware')
 const WebpackConfig = require('./webpack.config')
@@ -169,6 +170,20 @@ function registerMoreRouter() {
   router.post('/more/upload', function (req, res) {
     console.log(req.body, req.files)
     res.end('upload success!')
+  })
+
+  router.post('/more/post', function (req, res) {
+    const auth = req.headers.authorization
+    const [type, credentials] = auth.split(' ')
+    console.log(atob(credentials))
+
+    const [username, password] = atob(credentials).split(':')
+    if (type === 'Basic' && username === 'Yee' && password === '123456') {
+      res.json(req.body)
+    } else {
+      res.status(401)
+      res.end('UnAuthorization')
+    }
   })
 }
 
