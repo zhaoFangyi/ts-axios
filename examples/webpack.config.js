21695b3cf11e4bd210c556d6b87715e782824e60
zhaoFangyi <fangyieagle@qq.com>
2020-02-03 16:52:01 +0800

    feat: upload and download function
    and some insterting demo

--- examples/webpack.config.js
+++ examples/webpack.config.js
@@ -38,6 +38,12 @@ module.exports = {
             }
           }
         ]
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader', 'css-loader'
+        ]
       }
     ]
   },
